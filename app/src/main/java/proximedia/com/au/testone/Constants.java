85b41f21235a83d304605d86ac0088dbd366113e
proxima <mobiappsdevz@gmail.com>
2018-02-24 11:53:26 +1100

    Initial commit

---
+++ app/src/main/java/proximedia/com/au/testone/Constants.java
@@ -0,0 +1,10 @@
+package proximedia.com.au.testone;
+
+/**
+ * Created by pc on 22/02/2018.
+ */
+
+public interface Constants {
+    public static final String TAG = "testOne";
+    public static final String SELECTED_ITEM_POSITION = "selected_position";
+}
